4c7be31f2c740e7a6608b9b9172ecf6c9d1b0d68
Arif.A <arif.sain@gmail.com>
2025-07-15 13:23:17 +0200

    Enhance login functionality with validation and error messages for username and password

--- dist/pages/login.js
+++ dist/pages/login.js
@@ -1,8 +1,31 @@
-"use strict";
 const form = document.querySelector("form");
 const usernameInput = document.getElementById("username");
 const pswInput = document.getElementById("passowrd");
 const togglePswBtn = document.querySelector(".toggle-password");
 const usernameErrorMsg = document.getElementById("username-error-message");
-const passwordErrorMsg = document.getElementById("password-error-message");
+const pswErrorMsg = document.getElementById("password-error-message");
+form.addEventListener("submit", (e) => {
+    const username = usernameInput.value.trim();
+    const psw = pswInput.value.trim();
+    let valid = true;
+    const pswMinLength = 4;
+    if (username === "") {
+        usernameErrorMsg.style.display = "block";
+        valid = false;
+    }
+    else
+        usernameErrorMsg.style.display = "none";
+    if (psw.length < pswMinLength) {
+        pswErrorMsg.style.display = "block";
+        pswErrorMsg.textContent = "Lösenord måste innehålla minst 4 tecken.";
+        valid = false;
+    }
+    else
+        pswErrorMsg.style.display = "none";
+    if (valid) {
+        window.name = username;
+        window.location.href = "dashboard.html";
+    }
+});
+export {};
 //# sourceMappingURL=Login.js.map
--- src/pages/Login.ts
+++ src/pages/Login.ts
@@ -1,3 +1,5 @@
+import { name } from "../services/variables";
+
 const form = document.querySelector("form") as HTMLFormElement;
 const usernameInput = document.getElementById("username") as HTMLInputElement;
 const pswInput = document.getElementById("passowrd") as HTMLInputElement;
@@ -7,6 +9,30 @@ const togglePswBtn = document.querySelector(
 const usernameErrorMsg = document.getElementById(
   "username-error-message"
 ) as HTMLParagraphElement;
-const passwordErrorMsg = document.getElementById(
+const pswErrorMsg = document.getElementById(
   "password-error-message"
 ) as HTMLParagraphElement;
+
+form.addEventListener("submit", (e) => {
+  const username = usernameInput.value.trim();
+  const psw = pswInput.value.trim();
+
+  let valid = true;
+  const pswMinLength = 4;
+
+  if (username === "") {
+    usernameErrorMsg.style.display = "block";
+    valid = false;
+  } else usernameErrorMsg.style.display = "none";
+
+  if (psw.length < pswMinLength) {
+    pswErrorMsg.style.display = "block";
+    pswErrorMsg.textContent = "Lösenord måste innehålla minst 4 tecken.";
+    valid = false;
+  } else pswErrorMsg.style.display = "none";
+
+  if (valid) {
+    (window as any).name = username;
+    window.location.href = "dashboard.html";
+  }
+});
