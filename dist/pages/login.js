65d537fc6111f660b7a09cc710aa915cec0cc42c
Arif.A <arif.sain@gmail.com>
2025-07-15 13:08:07 +0200

    Add form and input element selections for login functionality

--- dist/pages/login.js
+++ dist/pages/login.js
@@ -1,2 +1,8 @@
 "use strict";
+const form = document.querySelector("form");
+const usernameInput = document.getElementById("username");
+const pswInput = document.getElementById("passowrd");
+const togglePswBtn = document.querySelector(".toggle-password");
+const usernameErrorMsg = document.getElementById("username-error-message");
+const passwordErrorMsg = document.getElementById("password-error-message");
 //# sourceMappingURL=Login.js.map
--- src/pages/Login.ts
+++ src/pages/Login.ts
@@ -1,1 +1,12 @@
-
+const form = document.querySelector("form") as HTMLFormElement;
+const usernameInput = document.getElementById("username") as HTMLInputElement;
+const pswInput = document.getElementById("passowrd") as HTMLInputElement;
+const togglePswBtn = document.querySelector(
+  ".toggle-password"
+) as HTMLButtonElement;
+const usernameErrorMsg = document.getElementById(
+  "username-error-message"
+) as HTMLParagraphElement;
+const passwordErrorMsg = document.getElementById(
+  "password-error-message"
+) as HTMLParagraphElement;
